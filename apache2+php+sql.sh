71c77521e2fdbe10cd6b74d06545ffa32c85593a
borck <borck@Borckla-Yoga.Speedport_W_724V_Typ_A_05011603_00_007>
2015-12-19 02:56:37 +0100

    apache2 install script added

---
+++ apache2+php+sql.sh
@@ -0,0 +1,4 @@
+sudo apt-get -y install mariadb-server mariadb-client apache2 php5 libapache2-mod-php5
+sudo apt-cache search php5
+sudo apt-get -y install php5-mysqlnd php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-apcu
+sudo service apache2 restart
