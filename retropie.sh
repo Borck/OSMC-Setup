82c3a6bb5d5d614981d52c296d8f40f4687d48ac
Borck <borck@hotmail.de>
2015-12-21 14:01:37 +0100

    retropie added

---
+++ retropie.sh
@@ -0,0 +1,6 @@
+#chmod +x ./RetroPie/scripts/retrosmc-config.cfg
+
+wget https://raw.githubusercontent.com/mcobit/retrosmc/direct/install-retrosmc.sh
+chmod +x install-retrosmc.sh
+./install-retrosmc.sh
+
